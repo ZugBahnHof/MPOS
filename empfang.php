52d68e7ad99192006a27886cd3ef0a0952dc1bf7
Julian <leuckerj@gmail.com>
2018-03-07 17:39:53 +0100

    Anzahl und Artikelnummer werden in einer Tabelle angezeigt. Job für @hansegucker : Daten schon im Formular überprüfen und ergänzen.

--- empfang.php
+++ empfang.php
@@ -1,6 +1,7 @@
 <?php
-
+$site_title = "Rechnung";
 include 'inc/header.inc.php';
+echo "<h1 class=".$site_color_accent_text.">Rechnung</h1>";
 echo '
   <table class="striped">
     <thead>
@@ -12,14 +13,26 @@ echo '
     </thead>
     <tbody>';
 for ( $i = 0; $i < count( $_POST['Menge'] ); $i ++ ) {
+  //$art_data = [];
+  $anzahl = $_POST['Menge'];
+  //$art_name = $art_data['anzahl'];
+  $art_num = $_POST['Artikelnummer'];
+  //$E_preis = $art_data['anzahl'];
+  //$G_preis = $anzahl * $E_preis;
 	echo '
     <tr>
+      <td>'.$anzahl[$i].'</td>
+      <td>'.$art_name.'</td>
+      <td>'.$art_num[$i].'</td>
+      <td>'.$E_preis.'</td>
+      <td>'.$G_preis.'</td>
+    </tr>
     ';
 }
 echo '
     </tbody>
   </table>
   ';
-echo "Zu zahlen:";
+echo "<b>Zu zahlen:</b>";
+include 'inc/footer.inc.php';
 ?>
-
